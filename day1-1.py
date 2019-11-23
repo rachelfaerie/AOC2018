231413c1335083d81cf04204af9a504153fcf722
rachelfaerie <rachelfaerie9007@gmail.com>
2019-11-22 18:55:28 -0600

    Modified day1-1.py

--- day1-1.py
+++ day1-1.py
@@ -1,3 +1,7 @@
+# Advent of Code - Day 1 - Part 1
+# Reads numbers from txt file and adds and subtracts them from an integer variable
+
 input_file = open("day1-1.txt", "r")
+
 for line in input_file.readlines() :
-    print(line)
+    print(int(line.strip()))
